fe22e9225a6b8603f0624780a86ea3e22100a1fc
Santharam2211 <santharamsenthil57@gmail.com>
2026-06-09 18:35:45 +0530

    Delete backend directory

--- backend/src/routes/certificateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { downloadCertificate } = require('../controllers/certificateController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.get('/download/:regId', protect, downloadCertificate);
-
-module.exports = router;
